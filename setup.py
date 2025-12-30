d44c19ab175e404b92c9b4061f71b7625eef18be
sanketbhalsing25 <your_email@gmail.com>
2025-12-30 23:36:20 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name='arvaya_rag_chatbot',
+    version='0.1.0',
+    author='sanket',
+    author_email="sanketbhalsing25@gmail.com",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[]
+) 
